a39fd0b61d6582e697795102b9313145a18988ce
UserN0tFound <>
2022-12-11 19:35:37 +0100

    petite erreur du login et register réglé

--- api/config.js
+++ api/config.js
@@ -1,10 +1,6 @@
 module.exports = {
     secret: "",
-<<<<<<< HEAD
-    dbPath: "/Users/kevinbationo/Desktop/js-projet.db",
-=======
-    dbPath: "C:/Users/Steven/DataGripProjects/quizz/identifier.sqlite",
->>>>>>> 20e614c0c7723bf3da5a8b9b561cfc5c08018734
+    dbPath: "/Users/rch/DataGripProjects/BLOC 2/projetWeb2.sqlite",
     // add your own configurations
     
 }
