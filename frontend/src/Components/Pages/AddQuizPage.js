9a5bd30e8aaf154d5183f5f6f66423792ee5c0f1
Steven <stevenagbassah23@gmai.com>
2022-12-12 20:21:53 +0100

    Création du modèle quiz, création d'une route quiz dans le backend et poursuite de la page AddQuizPage dans le front

--- frontend/src/Components/Pages/AddQuizPage.js
+++ frontend/src/Components/Pages/AddQuizPage.js
@@ -14,26 +14,60 @@ function renderQuizForm() {
   const main = document.querySelector('main');
   const form = document.createElement('form');
   form.className = 'p-5';
-  const course = document.createElement('select')
+  const courses = document.createElement('select')
+  courses.id = 'courses';
   fetch('http://localhost:3000/index')
     .then((response) => response.json())
     .then((data) =>  {
       data.forEach(element => {
-        course.innerHTML += `<option value = ${element.name}> ${element.name}</option>`
+        courses.innerHTML += `<option value = ${element.name}> ${element.name}</option>`
       });
-      course.innerHTML += '<br>';
     }
   )
   const submit = document.createElement('input');
   submit.value = 'Ajouter';
   submit.type = 'submit';
   submit.className = 'btn btn-info';
-  form.appendChild(course);
+  form.appendChild(courses);
   form.appendChild(submit)
   main.appendChild(form);
-  /* form.addEventListener('submit', onRegister); */
+  form.addEventListener('submit', addQuizz); 
 
 }
+
+
+async function addQuizz(e) {
+    e.preventDefault();
+  
+    const courses = document.querySelector('#course');
+    const selectedIndex = courses.selectedIndex();
+    const selectedCourse = courses.options[selectedIndex];
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        selectedCourse
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  
+    const response = await fetch(`${process.env.API_BASE_URL}/quiz/add`, options);
+  
+    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+  
+    const authenticatedUser = await response.json();
+  
+    // eslint-disable-next-line no-console
+    console.log('Newly registered & authenticated user : ', authenticatedUser);
+    /*
+    setAuthenticatedUser(authenticatedUser);
+  
+    Navbar();
+  
+    Navigate('/');
+    */
+  }
 
 
 function renderQuizQuestionForm() {
