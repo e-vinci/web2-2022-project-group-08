0f6ab738b6a79c31786fc2f7189079e75ee232cc
Steven <stevenagbassah23@gmai.com>
2022-12-14 18:48:53 +0100

    Gestion des erreurs/alertes connexion inscription

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     secret: "",
-    dbPath: "/Users/kevinbationo/Desktop/web-project.db",
+    dbPath: "/Users/Steven/DatagripProjects/quizz/identifier.sqlite",
     // add your own configurations
     
 }
