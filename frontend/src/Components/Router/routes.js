11e806977795bebeb1a3db44cedc06dc31639cc7
Steven <stevenagbassah23@gmai.com>
2022-12-15 20:09:35 +0100

    Ajout page de quie

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -2,17 +2,18 @@
 import HomePage from '../Pages/HomePage';
 import Logout from '../Logout/Logout';
 import RegisterPage from '../Pages/RegisterPage';
-import Quizzs from '../Pages/Quizzs';
+import QuizPage from '../Pages/QuizPage';
 import userPage from '../Pages/userPage';
 import ConfigQuizPage from '../Pages/ConfigQuizPage';
 import AddQuizPage from '../Pages/AddQuizPage';
 import AdminPage from '../Pages/AdminPage';
 
+
 const routes = {
   '/': HomePage,
   '/login': RegisterPage,
   '/register': RegisterPage,
-  '/Quizzs': Quizzs,
+  '/quizPage': QuizPage,
   '/logout': Logout,
   '/configurationQuiz' : ConfigQuizPage,
   '/users' : userPage,
