4289c3dee63ca29a5ddec650fe7783eb4ac5c1f3
UserN0tFound <>
2022-12-16 22:45:32 +0100

    config

--- api/config.js
+++ api/config.js
@@ -1,7 +1,12 @@
 
 module.exports = {
     secret: "you",
-    dbPath: "C:/Users/Steven/DataGripProjects/quizz/identifier.sqlite"
+    dbPath: "/Users/rch/DataGripProjects/BLOC 2/projetWeb2.sqlite"
     // add your own configurations
     
 }
+
+
+
+
+/* /Users/rch/DataGripProjects/BLOC 2/projetWeb2.sqlite */
