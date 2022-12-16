0d9859e0cb5c92782151de28bfedc1a229c5a5e7
Manal <manal.hebbal@student.vinci.be>
2022-12-16 14:51:03 +0100

    conflits

--- api/config.js
+++ api/config.js
@@ -1,12 +1,7 @@
 
 module.exports = {
-<<<<<<< HEAD
     secret: "you",
     dbPath: "C:/Users/user/DataGripProjects/db_js.db"
-=======
-    secret: "",
-    dbPath: "/Users/rch/DataGripProjects/BLOC 2/projetWeb2.sqlite",
->>>>>>> b1c9fe892d775b0e3d8a5642d8d54ab5da23801e
     // add your own configurations
     
 }
