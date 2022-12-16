c21ccc1e5152a72066fe0261d67913fa305ad282
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2022-12-16 12:58:50 +0100

    change

--- frontend/src/Components/Pages/Quizzs.js
+++ frontend/src/Components/Pages/Quizzs.js
@@ -2,9 +2,9 @@
 // import { clearPage, renderPageTitle } from '../../utils/render';
 // import Navbar from '../Navbar/Navbar';
 // import Navigate from '../Router/Navigate';
-import { readAllQuizz , readRightAnswer } from '../../models/movies';
-import { isAuthenticated } from '../../utils/auths';
-
+import { readAllQuizz  } from '../../models/movies';
+import { isAuthenticated } from '../../utils/auths'; 
+/* import { clearPage } from '../../utils/render'; */
 
 
 const ViewQuestionPage = async () => {
@@ -25,66 +25,149 @@ const ViewQuestionPage = async () => {
 };
 
 function renderQuizzForm(quizz) {
-  if (quizz === undefined) {
-    return '<p class="p-5">No quizz yet : (</p>';
-  }
+ 
   const  htmlQuestions = document.querySelector('main');
 
-  quizz.questions.forEach(element => {
+  quizz.forEach(element => {
+   // const goodAnswer = readRightAnswer(element.question_id );
     const div = document.createElement('div');
+    console.log('herer');
+    console.log(element);
     htmlQuestions.appendChild(div);
     div.className='center';
     div.innerHTML= `<tr> <th> ${element.content} </th> </tr>`;
-    htmlQuestions.appendChild(readAnswersQuestions(element.question_id, quizz.answers));  
+    htmlQuestions.appendChild();  
   });
  return htmlQuestions;
   };
 
 
-  function readAnswersQuestions(idQuestion, answers){
-    const  htmlAnswers = document.createElement('table');
-    const goodAnswer = readRightAnswer(idQuestion);
-   
-    answers.forEach(answerTable =>{
-     
-      answerTable.forEach(answer => {
-        if(answer.question === idQuestion){
-          htmlAnswers.appendChild(checkRightAnswer(answer, goodAnswer));
-        }
-      });
-      
-    });
-   
-    return htmlAnswers;
-  }
 
 
-function checkRightAnswer(rep, goodAnswer){
-  const tr = document.createElement('tr');
-  const td = document.createElement('td');
-  // const main = document.querySelector('main');
-  td.id=rep.answers_id;
-  td.innerText = rep.content;  
-  tr.appendChild(td);
-  td.class = 'message';
+
+
  
-  // main.appendChild(divRep);
-  td.addEventListener('onclick',()=>{
-   if(rep.answers_id === goodAnswer.answers_id ){
-     td.className = 'btn btn-success';
-   }else{
-     td.class = 'btn btn-danger';
-     const message = document.querySelector('.message');
-     message.textContent = goodAnswer.good_answer_feedback
-    //  alert(goodAnswer.good_answer_feedback);
-   }
-  });
- 
-  return tr;
-}
+  
   const Quizzs = () =>{
     ViewQuestionPage();
   }
 export default Quizzs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const Quizzs = async () => {
+  clearPage();
+  const main = document.querySelector('main');
+  const title = document.createElement('h2');
+  title.innerHTML = 'Répondez aux questions'
+  main.appendChild(title);
+  renderQuizzForm();
+ 
+};
+
+async function renderQuizzForm(){
+  const questions = await readAllQuizz();
+  const main = document.querySelector('main');
+
+  questions.forEach(question => { 
+      const questionContainer = document.createElement('div');
+      questionContainer.innerHTML += question.content;
+        question.reponses.forEach(reponse => {
+        questionContainer.innerHTML += reponse.content;
+        });
+        main.appendChild(questionContainer);
+  });
+  
+
+
+
+} */
+
+/* async function renderQuizzForm() {
+ 
+  };
+
+  const questions = await readAllQuizz();
+  console.log(questions)
+  const main = document.querySelector('main');
+
+  let timeoutID;
+
+  const delayInMiliSeconds = 15 * 1000;
+
+  const  questionContainer = document.createElement('div');
+  questionContainer.id = 'questionContainer';
+  let goodAnswer ;
+
+  const button = document.createElement('button');
+  
+
+  questions.forEach(question => {
+    
+    goodAnswer = readRightAnswer(question.question_id);
+    questionContainer.innerHTML= question.content;
+
+      question.reponses.forEach(reponse =>{
+        const div = document.createElement('div');
+        div.id = reponse.answers_id ;
+
+        questionContainer.appendChild(div);
+        button.innerHTML = reponse.content
+        div.appendChild(button);
+        main.appendChild(div);
+        main.appendChild(questionContainer);
+        console.log(div);
+        
+            
+    }); 
+    const divRep = document.querySelector('div');
+    
+  
+   setTimeout(() => {
+        button.addEventListener('click', () => {
+          if (goodAnswer.answers_id === divRep.id) {
+             button.click = "changeColor('green')";
+             clearTimeout(timeoutID);
+          }
+      });
+    }, delayInMiliSeconds); 
+  });
+  main.appendChild(questionContainer);
+ return  questionContainer; */
+ 
 
   
